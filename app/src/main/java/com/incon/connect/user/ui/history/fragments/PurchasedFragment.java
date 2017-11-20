b0099279d7d6ad5ed7b00fb769c60609a0289094
sunil <sunil.b@techraq.com>
2017-11-21 01:13:39 +0530

    Options Functionality 1)  added Bottom Options in  Favourite Screen

--- app/src/main/java/com/incon/connect/user/ui/history/fragments/PurchasedFragment.java
+++ app/src/main/java/com/incon/connect/user/ui/history/fragments/PurchasedFragment.java
@@ -382,11 +382,13 @@ public class PurchasedFragment extends BaseTabFragment implements PurchasedContr
 
             } else  if (tag == 1 && topClickedText.equals(getString(
                     R.string.bottom_option_find_service_center))) {
+                AppUtils.shortToast(getActivity(), getString(R.string.coming_soon));
                 bottomOptions = new String[0];
                 topDrawables = new int[0];
 
             } else  if (tag == 2 && topClickedText.equals(getString(
                     R.string.bottom_option_service_request))) {
+                AppUtils.shortToast(getActivity(), getString(R.string.coming_soon));
                 bottomOptions = new String[0];
                 topDrawables = new int[0];
 
@@ -438,6 +440,7 @@ public class PurchasedFragment extends BaseTabFragment implements PurchasedContr
             }
             else if (tag == 3 && topClickedText.equals(getString(
                     R.string.bottom_option_past_history))) {
+                AppUtils.shortToast(getActivity(), getString(R.string.coming_soon));
                 bottomOptions = new String[0];
                 topDrawables = new int[0];
             }
@@ -456,11 +459,13 @@ public class PurchasedFragment extends BaseTabFragment implements PurchasedContr
             else if (tag == 6 && topClickedText.equals(getString(
                     R.string.bottom_option_feedback))) {
 //                showFeedBackDialog();
+                AppUtils.shortToast(getActivity(), getString(R.string.coming_soon));
                 bottomOptions = new String[0];
                 topDrawables = new int[0];
             }
             else if (tag == 7 && topClickedText.equals(getString(
                     R.string.bottom_option_suggestions))) {
+                AppUtils.shortToast(getActivity(), getString(R.string.coming_soon));
                 bottomOptions = new String[0];
                 topDrawables = new int[0];
             } else if (tag == 0 && topClickedText.equals(getString(
@@ -471,10 +476,12 @@ public class PurchasedFragment extends BaseTabFragment implements PurchasedContr
 
             } else if (tag == 1 && topClickedText.equals(getString(
                     R.string.bottom_option_find_service_center))) {
+                AppUtils.shortToast(getActivity(), getString(R.string.coming_soon));
                 bottomOptions = new String[0];
                 topDrawables = new int[0];
             } else if (tag == 2 && topClickedText.equals(getString(
                     R.string.bottom_option_service_request))) {
+                AppUtils.shortToast(getActivity(), getString(R.string.coming_soon));
                 bottomOptions = new String[0];
                 topDrawables = new int[0];
             } else if (tag == 1 && topClickedText.equals(getString(
@@ -484,6 +491,7 @@ public class PurchasedFragment extends BaseTabFragment implements PurchasedContr
                 topDrawables = new int[0];
             } else if (tag == 2 && topClickedText.equals(getString(
                     R.string.bottom_option_feedback))) {
+                AppUtils.shortToast(getActivity(), getString(R.string.coming_soon));
                 bottomOptions = new String[0];
                 topDrawables = new int[0];
 //                showFeedBackDialog();
@@ -630,6 +638,7 @@ public class PurchasedFragment extends BaseTabFragment implements PurchasedContr
                 showInformationDialog(itemFromPosition.getSpecialInstruction());
             }   else if (tag == 2 && topClickedText.equals(getString(
                     R.string.bottom_option_how_to_use))) {
+                AppUtils.shortToast(getActivity(), getString(R.string.coming_soon));
 //                showInformationDialog(itemFromPosition.getInformation());
             }  else if (tag == 3 && topClickedText.equals(getString(
                     R.string.bottom_option_description))) {
