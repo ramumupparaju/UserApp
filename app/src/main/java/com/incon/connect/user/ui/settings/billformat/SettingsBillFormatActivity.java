df601b06429ec8c4ae6f337245678e142d196214
ramu <moonzdreamindia@gmail.com>
2017-11-30 17:57:05 +0530

    1.set values to bill format

--- app/src/main/java/com/incon/connect/user/ui/settings/billformat/SettingsBillFormatActivity.java
+++ app/src/main/java/com/incon/connect/user/ui/settings/billformat/SettingsBillFormatActivity.java
@@ -9,6 +9,7 @@ import com.incon.connect.user.databinding.ActivitySettingsBillFormatBinding;
 import com.incon.connect.user.ui.BaseActivity;
 import com.incon.connect.user.ui.billformat.BillFormatContract;
 import com.incon.connect.user.ui.billformat.BillFormatPresenter;
+import com.incon.connect.user.utils.DateUtils;
 
 
 /**
@@ -36,6 +37,11 @@ public class SettingsBillFormatActivity extends BaseActivity implements BillForm
          productInfoResponse = new ProductInfoResponse();
         binding.setProductinforesponse(productInfoResponse);
         binding.setSettingsBillFormatActivity(this);
+        binding.textDopValues.setText(": " + DateUtils.convertMillisToStringFormat(productInfoResponse.getPurchasedDate(), DateFormatterConstants.DD_MM_YYYY));
+        binding.textBillIdValues.setText(": " + productInfoResponse.getWarrantyId());
+        binding.textInvoiceNoValues.setText(": " + productInfoResponse.getInvoiceNumber());
+
+
 
     }
 
