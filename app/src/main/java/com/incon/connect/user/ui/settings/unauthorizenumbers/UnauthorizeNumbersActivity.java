ed20e69878f69beeb6cb812d51e69da90b36e4d1
ramu <moonzdreamindia@gmail.com>
2018-03-02 23:50:49 +0530

    1.Created UnauthorozeNumberActivity 2.Added static data to recyclerview in UnauthorozeNumberActivity

--- app/src/main/java/com/incon/connect/user/ui/settings/unauthorizenumbers/UnauthorizeNumbersActivity.java
+++ app/src/main/java/com/incon/connect/user/ui/settings/unauthorizenumbers/UnauthorizeNumbersActivity.java
@@ -2,13 +2,17 @@ package com.incon.connect.user.ui.settings.unauthorizenumbers;
 
 import android.content.Context;
 import android.databinding.DataBindingUtil;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
 
 import com.incon.connect.user.R;
 import com.incon.connect.user.apimodel.components.FeedbackData;
+import com.incon.connect.user.callbacks.AlertDialogCallback;
 import com.incon.connect.user.callbacks.IClickCallback;
+import com.incon.connect.user.custom.view.AppAlertVerticalTwoButtonsDialog;
 import com.incon.connect.user.databinding.ActivityUnauthorizenumbersBinding;
 import com.incon.connect.user.ui.BaseActivity;
 import com.incon.connect.user.ui.settings.unauthorizenumbers.adapter.UnauthorizeSeNumbersAdapter;
@@ -24,6 +28,8 @@ public class UnauthorizeNumbersActivity extends BaseActivity {
     private UnauthorizeSeNumbersAdapter unauthorizeNumbersAdapter;
 
     private ActivityUnauthorizenumbersBinding binding;
+    private AppAlertVerticalTwoButtonsDialog deleteDialog;
+
 
     @Override
     protected int getLayoutId() {
@@ -40,7 +46,50 @@ public class UnauthorizeNumbersActivity extends BaseActivity {
         binding.setActivity(this);
 
         initViews();
+        initializeToolbar();
 
+    }
+
+    private void initializeToolbar() {
+        binding.toolbar.toolbarLeftIv.setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
+        binding.toolbar.toolbarLeftIv.setImageResource(R.drawable.ic_back_arrow);
+        binding.toolbar.toolbarRightIv.setImageResource(R.drawable.ic_option_delete);
+        binding.toolbar.toolbarLeftIv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        binding.toolbar.toolbarRightIv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDeleteDialog();
+            }
+        });
+    }
+
+    private void showDeleteDialog() {
+        deleteDialog = new AppAlertVerticalTwoButtonsDialog.AlertDialogBuilder(this, new
+                AlertDialogCallback() {
+                    @Override
+                    public void alertDialogCallback(byte dialogStatus) {
+                        switch (dialogStatus) {
+                            case AlertDialogCallback.OK:
+                                deleteDialog.dismiss();
+                                break;
+                            case AlertDialogCallback.CANCEL:
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }).title(getString(R.string.dialog_delete))
+                .button1Text(getString(R.string.action_cancel))
+                .button2Text(getString(R.string.action_ok))
+                .build();
+        deleteDialog.showDialog();
+        deleteDialog.setButtonBlueUnselectBackground();
+        deleteDialog.setCancelable(true);
     }
 
     private void initViews() {
